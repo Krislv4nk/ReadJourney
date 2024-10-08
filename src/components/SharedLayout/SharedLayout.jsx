28561a68899ec57b3ece00e0d0717058b827e5bf
Krislv4nk <kristinalev4enko89@gmail.com>
2024-10-08 20:30:39 +0200

    added root colors, component Container

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,14 +1,17 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from 'components/Header/Header';
+import Container from '../Container/Container';
 
 const SharedLayout = () => {
   return (
     <>
+      <Container>
       <Header />
       <Suspense fallback={null}>
         <Outlet />
-      </Suspense>
+        </Suspense>
+        </Container>
     </>
   );
 };
--- src/pages/AuthPages/SignUpPage.jsx
+++ src/pages/AuthPages/SignUpPage.jsx
@@ -1,11 +1,12 @@
 
-
+import Container from "../../components/Container/Container";
 
 const SignUpPage = () => {
   return (
     <div>
+      <Container>
       <h2>SignUpPage</h2>
-      
+      </Container>
     </div>
   );
 };
