9842a30ce858c7233f6f10a15e3060808bd0ad41
Krislv4nk <kristinalev4enko89@gmail.com>
2024-10-30 19:17:30 +0100

    restarting computer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,30 @@
 import { NavLink } from 'react-router-dom';
+import sprite from '../../assets/icons/sprite.svg';
+import css from './Header.module.css';
 
 export const Header = () => {
   return (
-    <div>
-      <nav>
-        <NavLink to="recommended" end>
+    <div className={css.wrapper}>
+      <div className={css.logoContainer}>
+                <svg className={css.logo}>
+          <use xlinkHref={`${sprite}#icon-logo`}></use>
+                        </svg>
+                        <h3 className={css.logoHeader}>read journey</h3>
+</div>
+      <nav className={css.navWrapper}>
+        <NavLink to="recommended" className={css.link}>
           Home
         </NavLink>
-        <NavLink to="library">
+        <NavLink to="library" className={css.link}>
           Library
         </NavLink>
-        {/* <NavLink to="signUp" aria-label="Sign Up">
-          SignUpPage
-        </NavLink>
-        <NavLink to="signIn" aria-label="Sign In">
-          SignInPage
-        </NavLink>
-        <NavLink to="forgot-password" aria-label="Forgot Password">
-          Forgot Password
-        </NavLink>
-        <NavLink to="forgot-password/:token" aria-label="Recover Password">
-          Recover Password
-        </NavLink> */}
+        
       </nav>
+
+      <div className={css.buttonWrapper}>
+        <button className={css.userButton} type='button'><span></span><span>User Name</span></button>
+        <button className={css.logOutBtn} type='button'>Log out</button>
+      </div>
     </div>
   );
 };
