747e3ff6e053c56c40ac9184a1530a111df1b97d
Krislv4nk <kristinalev4enko89@gmail.com>
2024-11-04 17:59:59 +0100

    dashboard recommended (min-width: 768px)

--- src/components/RecommendedBooks/RecommendedBooks.jsx
+++ src/components/RecommendedBooks/RecommendedBooks.jsx
@@ -1,11 +1,25 @@
 
-import css from './RecommendedBooks.module.css'
+import css from './RecommendedBooks.module.css';
+import sprite from '../../assets/icons/sprite.svg';
 
 
 export const RecommendedBooks = () => {
 
 
     return (
-        <div className={css.recommendedBooks}>RecommendedBooks</div>
+        <div className={css.recommendedBooks}>
+            <h2 className={css.recommendedTitle}>Recommended</h2>
+            <div className={css.recommendedBtnWrapper}>
+            <button type='button' className={css.recommendedBtn} title='Back'>
+                <svg className={css.recommendedIcon}>
+          <use xlinkHref={`${sprite}#icon-left`}></use>
+                        </svg></button>
+            <button type='button'className={css.recommendedBtn} title='Forward'>
+            <svg className={css.recommendedIcon}>
+          <use xlinkHref={`${sprite}#icon-right`}></use>
+                        </svg>
+            </button>
+            </div>
+        </div>
     )
 }
