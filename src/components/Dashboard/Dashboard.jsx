c1643655d73c9c7f129e3fa8b42938370d6c340f
Krislv4nk <kristinalev4enko89@gmail.com>
2024-11-05 19:38:55 +0100

    finished styles WelcomePage @media screen and (min-width: 768px)

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,20 +1,32 @@
 
 
-import {Filters} from './Recommended/Filters/Filters';
-import {AddBook} from './MyLibrary/AddBook/AddBook';
+import React, { useState, useEffect } from 'react';
+import { Filters } from './Recommended/Filters/Filters';
+import { AddBook } from './MyLibrary/AddBook/AddBook';
 import { QuoteBlock } from './Recommended/QuoteBlock/QuoteBlock';
-import {FunctionalDescription} from './Recommended/FunctionalDescription/FunctionalDescription';
+import { FunctionalDescription } from './Recommended/FunctionalDescription/FunctionalDescription';
 import { RecommendedBooks } from './MyLibrary/RecommendedBooks/RecommendedBooks';
 import css from './Dashboard.module.css';
 
 const Dashboard = ({ page }) => {
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 1440);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 1440);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className={css.dashboard}>
       {page === 'recommended' && (
         <>
           <Filters />
           <FunctionalDescription link="/library" />
-          <QuoteBlock />
+          {isLargeScreen && <QuoteBlock />}
         </>
       )}
       {page === 'library' && (
