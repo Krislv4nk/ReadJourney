c683b2c5e5a75359cb97e8c7dab932a66e33e3d2
Krislv4nk <kristinalev4enko89@gmail.com>
2024-12-10 17:38:37 +0100

    build on vercel

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import svgr from "vite-plugin-svgr";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
-  resolve: {
-    alias: {
-      src: path.resolve(__dirname, "src"),
-      components: path.resolve(__dirname, "src/components"),
-      pages: path.resolve(__dirname, "src/pages"),
-      assets: path.resolve(__dirname, "src/assets"),
-    },
-  },
-  base: '/ReadJourney/', 
-  build: {
-    outDir: 'dist', 
-  },
+      plugins: [react(), svgr()],
+      resolve: {
+        alias: {
+          src: '/src',
+          components: '/src/components',
+          pages: '/src/pages',
+          assets: '/src/assets',
+        },
+      },
+  base: '/Portfolio/',
 });
 
 
