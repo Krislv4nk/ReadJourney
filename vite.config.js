bdb46e6528e030ff86f327000cb8253faabb7761
Krislv4nk <kristinalev4enko89@gmail.com>
2024-12-10 19:37:26 +0100

    build vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react()],
-  base: './',
+  plugins: [react(), svgr()],
+      resolve: {
+        alias: {
+          src: '/src',
+          components: '/src/components',
+          pages: '/src/pages',
+          assets: '/src/assets'
+        },
+      },
+  base: '/ReadJourney/',
   build: {
     outDir: 'dist', 
   },
@@ -11,6 +20,8 @@ export default defineConfig({
     open: true, 
   },
 });
+
+
 
 
 
